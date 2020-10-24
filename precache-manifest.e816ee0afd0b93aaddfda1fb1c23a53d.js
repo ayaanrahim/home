8204dd8f4eb8994987641cef1c89e78a728c9d6a
ayaanrahim <ayaanarahim@gmail.com>
2020-10-23 21:44:51 -0500

    Updates

---
+++ precache-manifest.e816ee0afd0b93aaddfda1fb1c23a53d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "183bef9ce4c79912627fcde2961ac728",
+    "url": "/index.html"
+  },
+  {
+    "revision": "88d7d44951adbdd01cfc",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "5512c16260333319fbde",
+    "url": "/static/css/main.9fc689ea.chunk.css"
+  },
+  {
+    "revision": "88d7d44951adbdd01cfc",
+    "url": "/static/js/2.8fa335fd.chunk.js"
+  },
+  {
+    "revision": "80fa05aac9f80c71b5f99ecec6ead10d",
+    "url": "/static/js/2.8fa335fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5512c16260333319fbde",
+    "url": "/static/js/main.6667340d.chunk.js"
+  },
+  {
+    "revision": "c88671eb88bf6c28dee9",
+    "url": "/static/js/runtime-main.6a060e8a.js"
+  },
+  {
+    "revision": "ee59aa4fed9f53a8bd8e6cbcc640c7b4",
+    "url": "/static/media/resume.ee59aa4f.pdf"
+  }
+]);
